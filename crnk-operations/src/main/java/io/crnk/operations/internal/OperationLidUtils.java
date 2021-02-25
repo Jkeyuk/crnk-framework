dda1c75769ad87fe545ced5cb5066427e030c462
Jkeyuk <jonkero2@gmail.com>
2021-02-25 12:29:50 -0500

    Add parse lids per type

---
+++ crnk-operations/src/main/java/io/crnk/operations/internal/OperationLidUtils.java
@@ -0,0 +1,29 @@
+package io.crnk.operations.internal;
+
+import io.crnk.core.engine.http.HttpMethod;
+import io.crnk.operations.Operation;
+import io.crnk.operations.server.order.OrderedOperation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public final class OperationLidUtils {
+
+	public static Map<String, Set<String>> parseLidsPerType(List<OrderedOperation> operations) {
+		Map<String, Set<String>> map = new HashMap<>();
+		List<Operation> ops = operations.stream()
+				.map(OrderedOperation::getOperation)
+				.filter(o -> o.getOp().equalsIgnoreCase(HttpMethod.POST.name()))
+				.collect(Collectors.toList());
+		ops.forEach(o -> {
+			Set<String> orDefault = map.getOrDefault(o.getValue().getType(), new HashSet<>());
+			orDefault.add(o.getValue().getId());
+			map.put(o.getValue().getType(),orDefault);
+		});
+		return map;
+	}
+}
