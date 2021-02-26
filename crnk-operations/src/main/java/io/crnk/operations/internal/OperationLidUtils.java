91dbb5e50e57cbe3d8e8fdde7f5ef47170122287
Jkeyuk <jonkero2@gmail.com>
2021-02-26 07:34:57 -0500

    add resolveLids

--- crnk-operations/src/main/java/io/crnk/operations/internal/OperationLidUtils.java
+++ crnk-operations/src/main/java/io/crnk/operations/internal/OperationLidUtils.java
@@ -1,10 +1,14 @@
 package io.crnk.operations.internal;
 
+import io.crnk.core.engine.document.Relationship;
 import io.crnk.core.engine.document.Resource;
+import io.crnk.core.engine.document.ResourceIdentifier;
 import io.crnk.core.engine.http.HttpMethod;
+import io.crnk.core.utils.Nullable;
 import io.crnk.operations.Operation;
 import io.crnk.operations.server.order.OrderedOperation;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -29,7 +33,46 @@ public final class OperationLidUtils {
 	}
 
 	public static boolean hasLid(Resource resource, Map<String, Set<String>> lidsPerType) {
-		return lidsPerType.containsKey(resource.getType())
-				&& lidsPerType.get(resource.getType()).contains(resource.getId());
+		return hasLid(lidsPerType, resource.getType(), resource.getId());
 	}
+
+	public static boolean hasLid(Map<String, Set<String>> lidsPerType, String type, String id) {
+		return lidsPerType.containsKey(type) && lidsPerType.get(type).contains(id);
+	}
+
+	public static void resolveLids(
+			Map<String, Set<String>> lidsPerType,
+			Map<String, Relationship> relationships,
+			Map<String, String> lidPerId
+	) {
+		if (isEmpty(lidsPerType) || isEmpty(relationships) || isEmpty(lidPerId)) {
+			return;
+		}
+
+		relationships.forEach((f, r) -> {
+			Nullable<Object> data = r.getData();
+			if (data.isPresent()) {
+				if (data.get() instanceof Collection) {
+					r.getCollectionData().get().forEach(rId -> resolveLid(lidsPerType, lidPerId, rId));
+				} else {
+					resolveLid(lidsPerType, lidPerId, r.getSingleData().get());
+				}
+			}
+		});
+	}
+
+	private static void resolveLid(
+			Map<String, Set<String>> lidsPerType,
+			Map<String, String> lidPerId,
+			ResourceIdentifier resourceIdentifier
+	) {
+		if (hasLid(lidsPerType, resourceIdentifier.getType(), resourceIdentifier.getId())) {
+			resourceIdentifier.setId(lidPerId.get(resourceIdentifier.getId()));
+		}
+	}
+
+	private static <T, K> boolean isEmpty(Map<T, K> col) {
+		return col == null || col.isEmpty();
+	}
+
 }
