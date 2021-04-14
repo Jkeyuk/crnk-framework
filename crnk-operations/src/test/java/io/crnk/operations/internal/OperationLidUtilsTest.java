c05f8678a68560487d5c6b83dd03d68b254a5537
Jkeyuk <jonkero2@gmail.com>
2021-04-14 10:53:17 -0400

    Prepare for Pr
    add test case resolveLidsForRelations_OnValidInput_RelationsIdsResolved

--- crnk-operations/src/test/java/io/crnk/operations/internal/OperationLidUtilsTest.java
+++ crnk-operations/src/test/java/io/crnk/operations/internal/OperationLidUtilsTest.java
@@ -1,13 +1,18 @@
 package io.crnk.operations.internal;
 
+import com.google.common.collect.ImmutableMap;
+import io.crnk.core.engine.document.Relationship;
 import io.crnk.core.engine.document.Resource;
+import io.crnk.core.engine.document.ResourceIdentifier;
 import io.crnk.core.engine.http.HttpMethod;
+import io.crnk.core.utils.Nullable;
 import io.crnk.operations.Operation;
 import io.crnk.operations.server.order.OrderedOperation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
@@ -41,6 +46,38 @@ class OperationLidUtilsTest {
 		Assertions.assertTrue(stringSetMap.get(expectedType).contains(expectedLid));
 		Assertions.assertTrue(stringSetMap.get(expectedType2).contains(expectedLid2));
 		Assertions.assertTrue(stringSetMap.get(expectedType2).contains(expectedLid3));
+	}
+
+	@Test
+	void resolveLidsForRelations_OnValidInput_RelationsIdsResolved() {
+		String type = "fish";
+		String localId = "localId";
+		String expectedInternalId = "InternalId";
+
+		Map<String, Map<String, String>> trackedLids = ImmutableMap.of(type, ImmutableMap.of(localId, expectedInternalId));
+
+		ResourceIdentifier identifier = new ResourceIdentifier();
+		identifier.setType(type);
+		identifier.setLid(localId);
+
+		ResourceIdentifier collectionIdentifier = new ResourceIdentifier();
+		collectionIdentifier.setType(type);
+		collectionIdentifier.setLid(localId);
+
+		Relationship relationship = new Relationship();
+		relationship.setData(Nullable.of(identifier));
+
+		Relationship collectionRelation = new Relationship();
+		collectionRelation.setData(Nullable.of(Collections.singletonList(collectionIdentifier)));
+
+		Map<String, Relationship> relData = ImmutableMap.of(
+				"fishType", relationship,
+				"listOfFishTypes", collectionRelation);
+
+		OperationLidUtils.resolveLidsForRelations(trackedLids, relData);
+
+		Assertions.assertEquals(expectedInternalId, identifier.getId());
+		Assertions.assertEquals(expectedInternalId, collectionIdentifier.getId());
 	}
 
 	private static Resource newResource(String expectedType, String expectedLid) {
